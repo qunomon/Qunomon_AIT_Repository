0a6dcda5a420a975e8be5ffe691d77d50be138e7
watanaka-yuichi <watanaka.yuichi.gx@mesw.co.jp>
2025-11-07 16:05:53 +0900

    #4889 [AIT] eval_llm_bleu_score修正

--- deploy/container/repository/my_ait.py
+++ deploy/container/repository/my_ait.py
@@ -157,7 +157,7 @@ if not is_ait_launch:
     manifest_genenerator.set_ait_name('eval_llm_bleu_score')
     manifest_genenerator.set_ait_description('MLFlowを使用して、LLMモデルで問題領域の質問に対して回答し、その生成されたテキストの品質を評価します。LLM評価基準を用いて、回答テキストのBLEUスコアを計算し、テキストの質を数値化します。')
     manifest_genenerator.set_ait_source_repository('https://github.com/aistairc/Qunomon_AIT_Repository/tree/eval_llm_bleu_score')
-    manifest_genenerator.set_ait_version('1.1')
+    manifest_genenerator.set_ait_version('1.2')
     manifest_genenerator.add_ait_licenses('Apache License Version 2.0')
     manifest_genenerator.add_ait_keywords('LLM')
     manifest_genenerator.add_ait_keywords('MLFlow')
