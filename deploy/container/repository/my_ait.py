6297759ba227503f97041b600fa213464ec11249
watanaka-yuichi <watanaka.yuichi.gx@mesw.co.jp>
2025-11-13 13:42:59 +0900

    #4890 [AIT] eval_llm_rouge_score修正

--- deploy/container/repository/my_ait.py
+++ deploy/container/repository/my_ait.py
@@ -163,7 +163,7 @@ if not is_ait_launch:
     manifest_genenerator.set_ait_name('eval_llm_rouge_score')
     manifest_genenerator.set_ait_description('MLFlowを使用して、LLMモデルでテキストに対してレジュメ生成し、その生成されたテキストの品質を評価します。LLM評価基準を用いて、テキストのROUGEスコアを計算し、テキストの質を数値化します。')
     manifest_genenerator.set_ait_source_repository('https://github.com/aistairc/Qunomon_AIT_Repository/tree/eval_llm_rouge_score')
-    manifest_genenerator.set_ait_version('1.1')
+    manifest_genenerator.set_ait_version('1.2')
     manifest_genenerator.add_ait_licenses('Apache License Version 2.0')
     manifest_genenerator.add_ait_keywords('LLM')
     manifest_genenerator.add_ait_keywords('MLFlow')
